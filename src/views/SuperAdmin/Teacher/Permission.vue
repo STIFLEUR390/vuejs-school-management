8c44b6b7a1e21c3001a495b1e21a60ceeaa48bf2
STIFLEUR390 <heroldtamko39@gmail.com>
2021-12-23 09:52:33 +0100

    Update Teacher permission

--- src/views/SuperAdmin/Teacher/Permission.vue
+++ src/views/SuperAdmin/Teacher/Permission.vue
@@ -45,18 +45,46 @@
                                             <tr style="background-color: #313a46; color: #ababab;">
                                                 <th>{{ $t('teacher') }}</th>
                                                 <th>{{ $t('marks') }}</th>
-                                                <th>{{ $t('assignment') }}</th>
+<!--                                                <th>{{ $t('assignment') }}</th>-->
                                                 <th>{{ $t('attendance') }}</th>
-                                                <th>{{ $t('online_exam') }}</th>
+<!--                                                <th>{{ $t('online_exam') }}</th>-->
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr v-for="(teacher, id) in teachers" :key="id">
                                                 <td>{{ teacher.user.name }}</td>
-                                                <td v-if="teacher.permissions.length >=1">{{ teacher.user.name }}</td>
-                                                <td v-if="teacher.permissions.length >=1">{{ teacher.user.name }}</td>
-                                                <td v-if="teacher.permissions.length >=1">{{ teacher.user.name }}</td>
-                                                <td v-if="teacher.permissions.length >=1">{{ teacher.user.name }}</td>
+                                                <td>
+                                                    <div class="form-group">
+                                                        <div class="custom-control custom-switch">
+                                                            <input type="checkbox" @change="changePermission(id, 'marks', teacher.permissions[0].marks)" class="custom-control-input" :id="'marks'+id" v-model="teacher.permissions[0].marks">
+                                                            <label class="custom-control-label" :for="'marks'+id">{{ $t('marks', teacher.permissions[0].marks) }}</label>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                               <!-- <td>
+                                                    <div class="form-group">
+                                                        <div class="custom-control custom-switch">
+                                                            <input type="checkbox" @change="changePermission(id, 'assignment', teacher.permissions[0].assignment)" class="custom-control-input" :id="'assignment'+id" v-model="teacher.permissions[0].assignment">
+                                                            <label class="custom-control-label" :for="'assignment'+id">{{ $t('assignment') }}</label>
+                                                        </div>
+                                                    </div>
+                                                </td>-->
+                                                <td>
+                                                    <div class="form-group">
+                                                        <div class="custom-control custom-switch">
+                                                            <input type="checkbox" @change="changePermission(id, 'attendance', teacher.permissions[0].attendance)" class="custom-control-input" :id="'attendance'+id" v-model="teacher.permissions[0].attendance">
+                                                            <label class="custom-control-label" :for="'attendance'+id">{{ $t('attendance') }}</label>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <!--<td>
+                                                    <div class="form-group">
+                                                        <div class="custom-control custom-switch">
+                                                            <input type="checkbox" @change="changePermission(id, 'online_exam', teacher.permissions[0].online_exam)" class="custom-control-input" :id="'online_exam'+id" v-model="teacher.permissions[0].online_exam">
+                                                            <label class="custom-control-label"  :for="'online_exam'+id">{{ $t('online_exam') }}</label>
+                                                        </div>
+                                                    </div>
+                                                </td>-->
                                             </tr>
                                         </tbody>
                                     </table>
@@ -76,6 +104,7 @@
     import 'admin-lte/plugins/jquery/jquery'
     import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle'
     import 'admin-lte/plugins/sweetalert2/sweetalert2'
+    import 'admin-lte/plugins/bs-custom-file-input/bs-custom-file-input'
     import $ from "jquery"
 
     import classSection from '../../../components/select2/ClassSelect'
@@ -105,6 +134,7 @@
                 this.classe = classe
             },
             getTeacherPermission(){
+                this.teachers = []
                 this.axios.get("/superadmin/crud?getData=all_teacher_permissions&class_id="+ this.classe.class_id+"&section_id="+this.classe.section_id)
                     .then((response) => {
                         this.teachers = response.data.data
@@ -124,14 +154,50 @@
             },
             assignValue(){
                 let length = this.teachers.length
-                console.log(length)
                 for (let i = 0; i <length ; i++) {
-                    console.log(this.teachers[i].permissions.length)
                     if (this.teachers[i].permissions.length < 1){
                         this.teachers[i].permissions[0] = this.templatePermission
                     }
                 }
             },
+            changePermission(id, column, val){
+                let newVal = 0
+                if (val){
+                    newVal = 1
+                }
+                let data = {
+                    getData: 'teacher_permissions',
+                    class_id: this.classe.class_id,
+                    section_id: this.classe.section_id,
+                    teacher_id: this.teachers[id].id,
+                    column_name: column,
+                    value: newVal,
+                }
+                this.axios.post("/superadmin/crud", data)
+                .then((response) => {
+                    $(document).Toasts('create', {
+                        class: 'bg-success',
+                        title: this.$t('success'),
+                        // subtitle: 'Subtitle',
+                        autohide: true,
+                        delay: 3000,
+                        body: this.$t(response.data.data.notification),
+                        // icon: "fas fa-envelope fa-lg"
+                    })
+                })
+                    .catch(() => {
+                        $(document).Toasts('create', {
+                            class: 'bg-danger',
+                            title: this.$t('error'),
+                            // subtitle: 'Subtitle',
+                            autohide: true,
+                            delay: 3000,
+                            body: this.$t('please_select_a_class_and_section'),
+                            // icon: "fas fa-envelope fa-lg"
+                        })
+                    })
+
+            }
         },
     }
 </script>
